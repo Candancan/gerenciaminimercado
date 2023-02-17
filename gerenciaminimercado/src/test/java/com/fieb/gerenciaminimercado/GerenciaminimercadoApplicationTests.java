331e3c6e92dbe50dc4d058cbfbfc93ef828d2673
Candancan <gabriel.can.ferraz@gmail.com>
2023-02-16 22:03:20 -0300

    Commit

---
+++ gerenciaminimercado/src/test/java/com/fieb/gerenciaminimercado/GerenciaminimercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fieb.gerenciaminimercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaminimercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
